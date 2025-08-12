33512574b6114046d558e81b25db185e15ccae2e
jonparedes30 <jonathanparedes738@gmail.com>
2025-08-12 16:25:46 -0500

    Remove conflicting imports

--- empresa/apps.py
+++ empresa/apps.py
@@ -6,10 +6,4 @@ class EmpresaConfig(AppConfig):
     name = 'empresa'
     
     def ready(self):
-        # Importar modelos adicionales para que Django los detecte
-        try:
-            from . import models_aprendizaje
-            from . import models_gamificacion  
-            from . import models_simulaciones
-        except ImportError:
-            pass
+        pass
