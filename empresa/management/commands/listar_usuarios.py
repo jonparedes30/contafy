474d2a8b5bcc8b4cde4c18ccab06d25706b5e36c
jonparedes30 <jonathanparedes738@gmail.com>
2025-08-21 17:11:41 -0500

    Fix: Usar modelo Usuario personalizado

--- empresa/management/commands/listar_usuarios.py
+++ empresa/management/commands/listar_usuarios.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from empresa.models import Empresa
+
+User = get_user_model()
 
 class Command(BaseCommand):
     help = 'Lista todos los usuarios registrados y sus empresas'
