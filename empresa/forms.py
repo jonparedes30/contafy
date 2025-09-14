3cbf6ec554fee177e972893b37294b9bcb78a000
jonparedes30 <jonathanparedes738@gmail.com>
2025-09-14 14:21:20 -0400

    fix: Add missing forms to resolve import error

--- empresa/forms.py
+++ empresa/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import CuentaContable, Capital
+from .models import CuentaContable, Capital, Empresa, Usuario
 from .services.accounting_setup import DEFAULT_CONTRAPARTIDAS, ensure_contrapartidas_for_account
 
 class CuentaContableForm(forms.ModelForm):
@@ -36,3 +36,36 @@ class CapitalForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.empresa = kwargs.pop('empresa', None)
         super().__init__(*args, **kwargs)
+
+class EmpresaForm(forms.ModelForm):
+    class Meta:
+        model = Empresa
+        fields = ['nombre', 'ruc', 'direccion', 'categoria']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'ruc': forms.TextInput(attrs={'class': 'form-control'}),
+            'direccion': forms.TextInput(attrs={'class': 'form-control'}),
+            'categoria': forms.Select(attrs={'class': 'form-select'}),
+        }
+
+class EmpleadoEmpresaForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ['username', 'email', 'first_name', 'last_name']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+class EditarEmpresaForm(forms.ModelForm):
+    class Meta:
+        model = Empresa
+        fields = ['nombre', 'direccion', 'categoria', 'tipo_negocio']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'direccion': forms.TextInput(attrs={'class': 'form-control'}),
+            'categoria': forms.Select(attrs={'class': 'form-select'}),
+            'tipo_negocio': forms.TextInput(attrs={'class': 'form-control'}),
+        }
