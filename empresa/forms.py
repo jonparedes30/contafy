a0615ae5ba6fe8a674c0658ebaeedf1976fc7950
jonparedes30 <jonathanparedes738@gmail.com>
2025-09-16 01:16:09 -0400

    Corregir CuentaContableForm - manejar parámetro empresa

--- empresa/forms.py
+++ empresa/forms.py
@@ -4,25 +4,18 @@ from .models import CuentaContable, Capital, Empresa, Usuario, Producto, Venta, 
 from .services.accounting_setup import DEFAULT_CONTRAPARTIDAS, ensure_contrapartidas_for_account
 
 class CuentaContableForm(forms.ModelForm):
-    contrapartidas_sugeridas = forms.MultipleChoiceField(
-        choices=[],
-        widget=forms.CheckboxSelectMultiple,
-        required=False,
-        label="Contrapartidas sugeridas"
-    )
-    
     class Meta:
         model = CuentaContable
         fields = ['nombre', 'tipo', 'monto_inicial']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'tipo': forms.Select(attrs={'class': 'form-select'}),
+            'monto_inicial': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01'}),
+        }
     
     def __init__(self, *args, **kwargs):
+        self.empresa = kwargs.pop('empresa', None)
         super().__init__(*args, **kwargs)
-        # Generar opciones de contrapartidas
-        choices = []
-        for conf in DEFAULT_CONTRAPARTIDAS:
-            nombre = conf["nombre_tpl"].format(base_nombre="[Nombre de cuenta]")
-            choices.append((conf["codigo_suffix"], f"{nombre} ({conf['tipo']})"))
-        self.fields['contrapartidas_sugeridas'].choices = choices
 
 class CapitalForm(forms.ModelForm):
     class Meta:
